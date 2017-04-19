763a93fdf2a9222303ae989bcca2c55d5cebef2f
DESKTOP-DOASQLK\lusuu <parapro.kh@gmail.com>
2017-04-19 20:17:01 +0300

    add files 14.04.2017

--- sigma/19042017/src/js/partials/burger-menu.js
+++
@@ -1,4 +0,0 @@
-$('.burger-menu').on('click', function() {
-	$('.burger-menu').toggleClass('change');
-	$('.header .navigation-holder').toggleClass('active');
-});
--- sigma/19042017/src/js/partials/main-search.js
+++
@@ -1,6 +0,0 @@
-$('.main-search-btn').on('click', function() {
-	$('.main-search-btn').toggleClass('active');
-	$('.main-search-input-box').toggleClass('active');
-	$('.navigation-holder .navigation').toggleClass('hide');
-	$('.navigation-holder').toggleClass('active');
-});
